56f98ebe6eff4dca436793c5a9a133cff791d7ed
Rahul <Rahulcs2699@gmail.com>
2024-08-25 22:10:21 +0530

    added js

--- page.js
+++ page.js
@@ -2,10 +2,10 @@ var sidenav = document.querySelector(".side-navbar")
 
 function showNavbar()
 {
-    sidenav.style.display = "block"
+    sidenav.style.left = "0"
 }
 
 function closeNavbar()
 {
-    sidenav.style.display = "none"
+    sidenav.style.left = "-60%"
 }
